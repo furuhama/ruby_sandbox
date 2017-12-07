afb71af23b52184529c4a5bbae904fba966e6199
furuhama <furuhama@users.noreply.github.com>
2017-12-07 12:15:17 +0900

    make for_loop ruby scripts to measure the difference between `!!` and `.!.!`

---
+++ for_loop_1.rb
@@ -0,0 +1,8 @@
+# testing the time for loop
+
+t = 'test text'
+
+for i in 1..100000000 do
+  a = !! t
+end
+
---
+++ for_loop_2.rb
@@ -0,0 +1,9 @@
+# testing for loop
+# use .!.!
+
+t = 'test text'
+
+for i in 1..100000000 do
+  a = t.!.!
+end
+
