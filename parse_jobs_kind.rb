9c045c032101b1babdc2eff98cd32fd8016acc03
furuhama <furuhama.yusuke@moneyforward.co.jp>
2018-02-14 17:23:38 +0900

    Add parse script

---
+++ parse_jobs_kind.rb
@@ -0,0 +1,16 @@
+# parse 日本標準産業分類
+
+require 'open-uri'
+require 'nokogiri'
+
+url = 'http://www.soumu.go.jp/toukei_toukatsu/index/seido/sangyo/02toukatsu01_03000044.html'
+
+html = open(url).read
+doc = Nokogiri::HTML(html, url)
+
+gyoshu = doc.xpath('//h3') # Nokogiti::XML::NodeSet
+
+gyoshu.each do |node|
+  str = node.children.text
+  puts str[8, 100] # 100 is larger than the length of every words
+end
