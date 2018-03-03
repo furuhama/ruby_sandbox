66a964fc7380e0e73b45feb04c33b936a0771b5e
furuhama <furuhama@users.noreply.github.com>
2018-03-03 16:08:15 +0900

    Update eval_test.eb

--- eval_test.rb
+++ eval_test.rb
@@ -1,5 +1,72 @@
-if __FILE__ == $0
+#!/usr/bin/env ruby
+
+def basic_eval
   eval "p 'hoge'"
-  fuga = 0
-  "#{fuga}"
+end
+
+class Language
+  def initialize(options={})
+    options.each do |name, value|
+      eval "@#{name} = '#{value}'"
+      eval "def #{name}; @#{name};end"
+    end
+  end
+
+  def my_define(options={})
+    options.each do |name, value|
+      eval <<-EOS
+        def print_#{name}
+          p '#{value}'
+        end
+      EOS
+    end
+  end
+end
+
+class Piyo; end
+
+p1 = Piyo.new
+p2 = Piyo.new
+
+p1.instance_eval do
+  def oden
+    p 'oden tabetai'
+  end
+end
+
+Piyo.class_eval do
+  def roar
+    p 'ugaaaa'
+  end
+end
+
+class SecretInfo
+  private
+
+  def details
+    'REALLY SECRET'
+  end
+end
+
+def books(prefix)
+  lambda {|titles| titles.map {|title| "#{prefix} #{title}" } }
+end
+
+if __FILE__ == $0
+  ll = Language.new({haskell: 'functional'})
+  p ll.haskell
+  ll.my_define({ruby: 'object-oriented'})
+  ll.print_ruby
+
+  p1.oden
+  # p2.oden
+
+  p1.roar
+  p2.roar
+
+  s = SecretInfo.new
+  p s.instance_eval { details }
+
+  my_book_maker = books "Effective"
+  p my_book_maker.call ["C++", "Ruby"]
 end
