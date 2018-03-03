5171a5b26cc2d374da6183e6d768deb01ed4e09d
furuhama <furuhama@users.noreply.github.com>
2018-03-03 13:03:18 +0900

    Improve nqueen

--- nqueen.rb
+++ nqueen.rb
@@ -1,127 +1,5 @@
 #/usr/bin/env ruby
 
-# N-Queen solver
-
-# class Queen
-#   attr_reader :col, :row
-
-#   def initialize(col, row)
-#     @col = col
-#     @row = row
-#   end
-# end
-
-# class Board
-#   attr_reader :size
-#   attr_accessor :board, :queens
-
-#   def initialize(size=4)
-#     @size = size
-#     @board = Array.new(size, Array.new(size, true)) # @board[col][row]
-#     @queens = []
-#   end
-
-#   def solver
-#     # if next_step
-#     #   return queens
-#     # end
-
-#     next_step
-#   end
-
-#   private
-
-#   def next_step
-#     if queens.length == size
-#       return true
-#     end
-
-#     begin
-#       search_column(queens.length)
-
-#       latest_queen = queens[-1]
-#       p latest_queen
-#       update_board(latest_queen.col, latest_queen.row)
-#       p board
-
-#       # next_step
-#     rescue
-#       if queens.length == 0
-#         return false
-#       end
-
-#       latest_queen = queens[-1]
-#       board[latest_queen.col][latest_queen.row] = false
-
-#       queens.delete_at(-1)
-
-#       # next_step
-#     end
-#   end
-
-#   def search_column(col_num)
-#     (0..size).each do |row_num|
-#       if board[col_num][row_num]
-#         queens << Queen.new(col_num, row_num)
-
-#         break
-#       end
-
-#       if row_num == size
-#         raise 'there is no possibility'
-#       end
-#     end
-#   end
-
-#   def update_board(col_num, row_num)
-#     # 横方向
-#     # (col_num+1..size).each do |i|
-#     #   board[i][row_num] = false
-#     # end
-
-#     # 右上方向
-#     # (col_num+1..size).each do |i|
-#     #   row_diff = i - col_num
-#     #   if (row_num - row_diff) >= 0
-#     #     board[i][(row_num - row_diff)] = false
-#     #   else
-#     #     break
-#     #   end
-#     # end
-
-#     # 右下方向
-#     # (col_num+1..size).each do |i|
-#     #   row_diff = i - col_num
-#     #   if (row_num + row_diff) > size - 1
-#     #     board[i][(row_num + row_diff)] = false
-#     #   else
-#     #     break
-#     #   end
-#     # end
-
-#     rest = size - 1 - col_num
-
-#     # ここでうまくいってないっぽい
-#     (1..rest+1).each do |i|
-#       board[col_num+i][row_num] = false
-#       if row_num - i >= 0
-#         board[col_num+i][row_num-i] = false
-#       end
-#       if row_num + i < size
-#         board[col_num+i][row_num+i] = false
-#       end
-#     end
-#   end
-# end
-
-# if __FILE__ == $0
-  # b = Board.new
-
-  # b.solver
-# end
-
-# うまくいきそう
-
 class NQueen
   def initialize(size = 4)
     @size = size
@@ -129,23 +7,61 @@ class NQueen
   end
 
   def printer
-    text = ""
-    (0..@size-1).each do |col|
-      (0..@size-1).each do |row|
-        text += if @queens[row] == col
-                  "Q "
-                else
-                  "_ "
-                end
+    @queens.each do |queen|
+      text = ""
+      (0..@size-1).each do |col|
+        (0..@size-1).each do |row|
+          text += if queen[row] == col
+                    "Q "
+                  else
+                    "_ "
+                  end
+        end
+        text += "\n"
       end
-      text += "\n"
+      puts text
     end
-    puts text
+  end
+
+  def solver
+    generator
+
+    # p @queens.select { |arr| checker(arr) }
+    p @queens.select! { |arr| arr[0] == 1 }
+  end
+
+  def generator
+    def permutator(unused, used)
+      unless unused.empty?
+        for e in unused
+          used_clone = used.clone
+          used_clone << e
+
+          unused_clone = unused.clone
+          unused_clone.delete(e)
+
+          permutator(unused_clone, used_clone)
+        end
+      else
+        @queens << used
+      end
+    end
+    permutator((0..@size-1).to_a, [])
+  end
+
+  def checker(board)
+    (0..@size-2).each do |element|
+      for pair in (element+1..@size-1)
+        if (board[element] - board[pair]).abs == element - pair
+          return false
+        end
+      end
+    end
+    return true
   end
 end
 
 # main process
 if __FILE__ == $0
-  hoge = NQueen.new
-  hoge.printer
+  NQueen.new.solver
 end
