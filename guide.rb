9c441896359037c6951a5787b13b80218ff5996e
furuhama <furuhama@users.noreply.github.com>
2018-03-05 01:23:26 +0900

    Add singleton_method

--- guide.rb
+++ guide.rb
@@ -23,8 +23,17 @@ class C
   end
 end
 
+c_sin = C.new
+
+# 特異メソッド(a.k.a. singleton method)
+def c_sin.my_hello
+  p 'this is singleton method'
+end
+
 if __FILE__ == $0
   c = C.new
 
   c.caller
+
+  c_sin.my_hello
 end
