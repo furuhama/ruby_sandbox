5132fb6704893fe25080f8d6a97c607e9a405399
furuhama <furuhama@users.noreply.github.com>
2019-02-09 20:37:26 +0900

    Try delegate method

---
+++ lib/delegate/with_same_method_name.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'forwardable'
+
+class ParentCat
+  def mew
+    puts 'mew from parent'
+  end
+end
+
+class ChildCat < ParentCat
+  extend Forwardable
+
+  delegate mew: :@delegator
+
+  def initialize
+    @delegator = ParentCat.new
+  end
+
+  def mew
+    puts 'mew from child'
+  end
+end
+
+child_cat = ChildCat.new
+child_cat.mew # mew from child
